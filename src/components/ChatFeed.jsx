8a7937c6a455b6991baeec3f48487d9e7e468c17
Harshit Gupta <iamharshit999@gmail.com>
2024-07-13 02:07:15 +0530

    Project Started

---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,17 @@
+import MessageForm from "./MessageForm";
+import MyMessage from "./MyMessage";
+import TheirMessage from "./TheirMessage";
+
+const ChatFeed = (props) => {
+    const { chats, activeChat, messages, userName } = props;
+
+    const chat = chats && chats[activeChat];
+    console.log(chat,userName,messages);
+    return (
+        <div>
+            ChatFeed
+        </div>
+    )
+}
+
+export default ChatFeed
