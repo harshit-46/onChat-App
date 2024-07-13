a55037b5d7559e625583ae1d18adbc3d7dd3d838
Harshit Gupta <iamharshit999@gmail.com>
2024-07-13 13:12:08 +0530

    components added

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+const MyMessage = ({message}) => {
 
-const MyMessage = () => {
+    if(message?.attachments?.lenght > 0) {
+        return(
+            <img
+                src={message.attachments[0].file}
+                alt="message-attachment"
+                className="message-image"
+                style={{float: 'right'}}
+            />
+        )
+    }
+
     return (
-        <div>
-
+        <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '3B2A50' }}>
+            {message.text}
         </div>
     )
 }
