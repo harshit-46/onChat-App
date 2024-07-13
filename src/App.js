36248fc66265f16bfa191f69362d48a382f518eb
Harshit Gupta <iamharshit999@gmail.com>
2024-07-13 18:58:40 +0530

    Files added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import { ChatEngine } from "react-chat-engine";
-import './App.css';
+import { ChatEngine } from 'react-chat-engine';
+
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
 
 const App = () => {
+    if (!localStorage.getItem('username')) return <LoginForm />;
+
     return (
         <ChatEngine
             height="100vh"
-            projectID="ddfdbb2b-8c0d-4f25-95d4-222a66b7adf4"
-            userName="Harshit25"
-            userSecret="HS461900hs"
-            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+            projectID={projectID}
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+            onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
         />
-    )
-}
+    );
+};
 
-export default App
+// infinite scroll, logout, more customizations...
+
+export default App;
