286236db5ef7df30d2c41ecb5300e3498aa7fb4d
Harshit Gupta <iamharshit999@gmail.com>
2024-07-13 23:57:44 +0530

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
 import './App.css';
 
-const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+const projectID = 'ddfdbb2b-8c0d-4f25-95d4-222a66b7adf4';
 
 const App = () => {
     if (!localStorage.getItem('username')) return <LoginForm />;
@@ -20,7 +20,5 @@ const App = () => {
         />
     );
 };
-
-// infinite scroll, logout, more customizations...
 
 export default App;
